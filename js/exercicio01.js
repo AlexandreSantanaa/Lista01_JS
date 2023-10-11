9488b78a94a3d43091eacbf61da01becd39f9b3c
AlexandreSantanaa <126908528+AlexandreSantanaa@users.noreply.github.com>
2023-10-11 00:38:15 -0300

    Create exercicio01.js

---
+++ js/exercicio01.js
@@ -0,0 +1,8 @@
+function executarExercicio01(){
+    alert('1) Escreva um algoritmo para ler um valor do teclado e exibir como resposta o número sucessor.')
+
+    let valor = parseInt(prompt("Digite um valor para receber seu sucessor"))
+
+    let sucessor = valor +1
+    alert("sucessor = " + sucessor)
+}
